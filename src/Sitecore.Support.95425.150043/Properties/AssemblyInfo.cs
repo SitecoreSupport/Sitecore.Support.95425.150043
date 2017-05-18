ea91322555d2a410a7deb8a0a8ec527e4936a2d3
DK\DTAT <dtat@sitecore.net>
2017-05-18 11:52:16 +0300

    Patch commit

---
+++ src/Sitecore.Support.95425.150043/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sitecore.Support.95425.150043")]
+[assembly: AssemblyProduct("Sitecore.Support.95425.150043")]
+[assembly: ComVisible(false)]
